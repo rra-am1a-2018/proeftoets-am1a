34c3e5017cb54e2397ed57bbdc28b6907a4082a3
Edgar Kalachian <Edgar-nathalie@hotmail.com>
2019-06-21 13:40:25 +0200

    versie 5

--- edgar_edgartje2002.php
+++ edgar_edgartje2002.php
@@ -1,1 +1,20 @@
-VERSIE 4
+<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>edgar</td>
+      <td>niks</td>
+      <img src="./img/.svg" />
+    </tr>
+    
+    </tr>
+  </tbody>
+</table>
