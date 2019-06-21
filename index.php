b382a00f9b7045731a28366c385735c72c94e6de
IJuanTM <iwan2510@gmail.com>
2019-06-21 13:58:27 +0200

    mijn index row

--- index.php
+++ index.php
@@ -1,52 +1,56 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Proeftoets-AM1A</h1>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">llnr</th>
-      <th scope="col">Voornaam</th>
-      <th scope="col">Githubnaam</th>
-      <th scope="col">Avatar (40x40)</th>
-      <th scope="col">link</th>
-      <th scope="col">Cijfer voor pagina</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Arjan</td>
-      <td>rra-am1a-2018</td>
-      <td>plaatje</td>
-      <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
-      <td>10+</td>
-    </tr>
-  </tbody>
-</table>
+  <title>Hello, world!</title>
+</head>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+<body>
+  <h1>Proeftoets-AM1A</h1>
+
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">llnr</th>
+        <th scope="col">Voornaam</th>
+        <th scope="col">Githubnaam</th>
+        <th scope="col">Avatar (40x40)</th>
+        <th scope="col">link</th>
+        <th scope="col">Cijfer voor pagina</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Arjan</td>
+        <td>rra-am1a-2018</td>
+        <td>plaatje</td>
+        <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
+        <td>10+</td>
+      </tr>
+      <tr>
+        <th scope="row">326650</th>
+        <td>Iwan</td>
+        <td>IJuanTM/td>
+        <td><img src="./img/IJuanTM.png" width="40px" height="40px;"></td>
+        <td><a href="./Iwan_IJuanTM.php">Naar Iwan's Site</a></td>
+        <td>11+</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
-
-
-
-
-
-
-
