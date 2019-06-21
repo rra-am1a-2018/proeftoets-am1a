e8e12260d683c435d3e6e54c54dc6d192f7a1027
Robin vd Hoeven <322961.ionic@gmail.com>
2019-06-21 13:34:53 +0200

    Added firstname to table

--- robin_J3wellZ.php
+++ robin_J3wellZ.php
@@ -1,3 +1,28 @@
 <?php
 echo "1 grote vuurbal jonge";
 ?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Proeftoets</title>
+</head>
+<body>
+<br><br><br>
+
+<table>
+    <thead>
+        <tr>
+            <th colspan="2">Proeftoets</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Firstname:</td>
+            <td>Robin</td>
+        </tr>
+    </tbody>
+</table>
+</body>
+
+</html>
