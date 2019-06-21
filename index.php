b3fc797ae0611b34062d704d7f3242d1d70da748
wub <gitwub@gmail.com>
2019-06-21 14:06:05 +0200

    al is ok

--- index.php
+++ index.php
@@ -1,184 +1,71 @@
 <!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Proeftoets-AM1A</h1>
 
-  <title>Hello, world!</title>
-</head>
-
-<body>
-  <h1>Proeftoets-AM1A</h1>
-
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">llnr</th>
-        <th scope="col">Voornaam</th>
-        <th scope="col">Githubnaam</th>
-        <th scope="col">Avatar (40x40)</th>
-        <th scope="col">link</th>
-        <th scope="col">Cijfer voor pagina</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Arjan</td>
-        <td>rra-am1a-2018</td>
-        <td>plaatje</td>
-        <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
-        <td>10+</td>
-      </tr>
-      <tr>
-        <th scope="row">320656</th>
-        <td>Dirk</td>
-        <td>dirkvansoest</td>
-        <td>plaatje</td>
-        <td><a href=".\img\slaap.png">mijn homepage</a></td>
-        <td>20</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
-</head>
-
-<body>
-  <h1>Proeftoets-AM1A</h1>
-
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">llnr</th>
-        <th scope="col">Voornaam</th>
-        <th scope="col">Githubnaam</th>
-        <th scope="col">Avatar (40x40)</th>
-        <th scope="col">link</th>
-        <th scope="col">Cijfer voor pagina</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Arjan</td>
-        <td>rra-am1a-2018</td>
-        <td>plaatje</td>
-        <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
-        <td>10+</td>
-      </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>Robin</td>
-        <td>J3wellZ</td>
-        <td>picture m8</td>
-        <td><a href="./robin_J3wellZ.php">mijn homepage</a></td>
-        <td>10+</td>
-        <th scope="row">320656</th>
-        <td>Dirk</td>
-        <td>dirkvansoest</td>
-        <td><img src="img\slaap.png" width="50px" height="50px"></td>
-        <td><a href="dirk_dirkvansoest.php">mijn homepage</a></td>
-        <td>20</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
-</head>
-
-<body>
-  <h1>Proeftoets-AM1A</h1>
-
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">llnr</th>
-        <th scope="col">Voornaam</th>
-        <th scope="col">Githubnaam</th>
-        <th scope="col">Avatar (40x40)</th>
-        <th scope="col">link</th>
-        <th scope="col">Cijfer voor pagina</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Arjan</td>
-        <td>rra-am1a-2018</td>
-        <td>plaatje</td>
-        <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
-        <td>10+</td>
-      </tr>
-      <tr>
-        <th scope="row">322085</th>
-        <td>Wob</td>
-        <td>Fatpike</td>
-        <td><img src="./img/FatpikeAvatar.png" height="40px" width="40px"></td>
-        <td><a href="./wob_Fatpike.php">mijn homepage</a></td>
-        <td>∞</td>
-      </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>Robin</td>
-        <td>J3wellZ</td>
-        <td>picture m8</td>
-        <td><a href="./robin_J3wellZ.php">mijn homepage</a></td>
-        <td>10+</td>
-      <tr>
-        <th scope="row">320656</th>
-        <td>Dirk</td>
-        <td>dirkvansoest</td>
-        <td><img src="img\slaap.png" width="50px" height="50px"></td>
-        <td><a href="dirk_dirkvansoest.php">mijn homepage</a></td>
-        <td>20</td>
-      </tr>
-    </tbody>
-  </table>
+    <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">llnr</th>
+      <th scope="col">Voornaam</th>
+      <th scope="col">Githubnaam</th>
+      <th scope="col">Avatar (40x40)</th>
+      <th scope="col">link</th>
+      <th scope="col">Cijfer voor pagina</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Arjan</td>
+      <td>rra-am1a-2018</td>
+      <td>plaatje</td>
+      <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
+      <td>10+</td>
+    </tr>
+    <tr>
+      <th scope="row">322085</th>
+      <td>Wob</td>
+      <td>Fatpike</td>
+      <td><img src="./img/FatpikeAvatar.png" height="40px" width="40px"></td>
+      <td><a href="./wob_Fatpike.php">mijn homepage</a></td>
+      <td>∞</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Robin</td>
+      <td>J3wellZ</td>
+      <td>picture m8</td>
+      <td><a href="./robin_J3wellZ.php">mijn homepage</a></td>
+      <td>10+</td>
+    <tr>
+      <th scope="row">320656</th>
+      <td>Dirk</td>
+      <td>dirkvansoest</td>
+      <td><img src="img\slaap.png" width="50px" height="50px"></td>
+      <td><a href="dirk_dirkvansoest.php">mijn homepage</a></td>
+      <td>20</td>
+    </tr>
+  </tbody>
+</table>
 
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
 
 
@@ -187,77 +74,3 @@
 
 
 
-<<<<<<< HEAD <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
-  </head>
-
-  <body>
-    <h1>Proeftoets-AM1A</h1>
-
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">llnr</th>
-          <th scope="col">Voornaam</th>
-          <th scope="col">Githubnaam</th>
-          <th scope="col">Avatar (40x40)</th>
-          <th scope="col">link</th>
-          <th scope="col">Cijfer voor pagina</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Arjan</td>
-          <td>rra-am1a-2018</td>
-          <td>plaatje</td>
-          <td><a href="./arjan_rra-am1c-2018.php">mijn homepage</a></td>
-          <td>10+</td>
-        </tr>
-        <tr>
-          <th scope="row">326650</th>
-          <td>Iwan</td>
-          <td>IJuanTM/td>
-          <td><img src="./img/IJuanTM.png" width="40px" height="40px;"></td>
-          <td><a href="./Iwan_IJuanTM.php">Naar Iwan's Site</a></td>
-          <td>11+</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-
-  </html>
-  =======
-  <<<<<<< HEAD <?php
-                echo "Hallo Git World!";
-                echo "Dit is nieuwe code voor de tweede commit";
-                echo "Dit is de derde commit";
-                echo "Dit wordt de vijfde commit";
-                echo "Dit is de zevende commit";
-                echo "Dit is de achtste commit";
-                echo "Dit is de negende commit";
-                echo "Dit is de tiende commit";
-                echo "Dit is de elfde commit";
-                echo "Dit is de twaalfde commit";
-                echo "Dit is de dertiende commit";
-                echo "Dit is de veertiende commit";
-
-                echo "Dit is een vijftiende commit";
-                echo "Dit is de zestiende commit";
-                echo "Pull request";
-                echo "Test";
-                ?>=======>>>>>>> f41de473e7661fc018d23767d2e217401a8f87b7
-    >>>>>>> 2ce1adaea10fb0425f2a8cdc1301565ed639910d
