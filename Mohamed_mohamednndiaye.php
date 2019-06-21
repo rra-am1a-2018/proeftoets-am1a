5d1e5689a5f3dbb19ec565bc473a69bf9ba02bbc
Mohamed <mohamedndiaye8030@gmail.com>
2019-06-21 13:33:00 +0200

    Dit is mijn eerst commit

--- Mohamed_mohamednndiaye.php
+++ Mohamed_mohamednndiaye.php
@@ -1,4 +1,30 @@
 <?php
-echo "Hallo Git World!";
+
 
 ?>
+
+<html>
+<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Voornaam</th>
+      <th scope="col">Tussenvoegsel</th>
+      <th scope="col">Achternaam</th>
+      <th scope="col">Hobby</th>
+      <th scope="col">Avatar</th>
+
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    
+  </tbody>
+</table>
+</html>
