104753d0dc01fd42d08e9750dc0cf9d636f9e2a3
wub <gitwub@gmail.com>
2019-06-21 13:33:09 +0200

    added table body

--- wob_Fatpike.php
+++ wob_Fatpike.php
@@ -24,6 +24,27 @@
           <th scope="col">Avatar</th>
         </tr>
       </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
