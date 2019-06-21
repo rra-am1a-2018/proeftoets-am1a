0374790f816d10badba2740db1da8f57c0eb7fb5
RiDiCage <rvdkooij@live.nl>
2019-06-21 13:38:29 +0200

    Add header in table

--- Richard_RiDiCage.php
+++ Richard_RiDiCage.php
@@ -28,30 +28,16 @@
         <table class="table table-dark">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                    <th scope="col">Voornaam</th>
+                    <th scope="col">Tussenvoegsel</th>
+                    <th scope="col">Achternaam</th>
+                    <th scope="col">Hobby</th>
+                    <th scope="col">Avatar</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
+
                 </tr>
             </tbody>
         </table>
